1c1521b936dbacf6ac835f321fc1e77664fa7bab
Jonas Rafael <115741544+jonas07br@users.noreply.github.com>
2024-04-29 11:38:27 -0300

    Add variaveis

--- Main/src/App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        for(int x = 0;x<10;x++){
-            System.out.println(x);
-        }
-    }
-}
---
+++ Main/src/ContaTerminal.java
@@ -0,0 +1,15 @@
+public class ContaTerminal {
+
+    public static void main(String[] args) throws Exception {
+        int op = 0;
+
+        float saldo = 0;
+        int numConta = 0;
+        String agencia = "00000";
+        String nomeCliente = "00000";
+
+        System.out.println("Hello, World!");
+        
+        }
+    }
+}
