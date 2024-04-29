fcbd9e9ab8012e6f13d8c128341a7a348e16d0cc
jonas07br <jonasrafa07@gmail.com>
2024-04-29 11:09:13 -0300

    Main

---
+++ Main/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        for(int x = 0;x<10;x++){
+            System.out.println(x);
+        }
+    }
+}
